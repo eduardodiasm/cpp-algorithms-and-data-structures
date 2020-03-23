cbf39c9c4ac5ede898d2b9e8d393d4c073536e91
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-22 22:49:20 -0300

    adding the print linked list function

--- linked_lists/implementation.cpp
+++ linked_lists/implementation.cpp
@@ -9,6 +9,15 @@ public:
     Node *next;
 };
 
+void print_list(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     Node* head = NULL;   
@@ -20,6 +29,16 @@ int main(int argc, char const *argv[])
     third = new Node();
 
     head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next= third;
+    
+    third->data = 3;
+    third->next = NULL;
+    
+    print_list(head);
+
 
     return 0;
 }
