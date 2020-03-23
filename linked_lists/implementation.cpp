c597fd688695042a0dbc8f8e23df024e53b70586
Eduardo Dias <eduardodiasprog@gmail.com>
2020-03-22 22:43:12 -0300

    adding the linked lists

---
+++ linked_lists/implementation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+int main(int argc, char const *argv[])
+{
+    Node* head = NULL;   
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+
+    return 0;
+}
